e823d958c25ef68ab0e74a0377eda6bdd5483969
ClaudioFritzen <sdfritzen96@gmail.com>
2022-08-30 02:21:42 +0100

    criando o templete do html

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+# pagina de cadastro e paghome
 def cadastro(request):
     return HttpResponse("Você está na pagina de cadastro")
-    pass
+
+# login
+
+def logar(request):
+    return HttpResponse("Você esta na pagina de login")
--- main/settings.py
+++ main/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY ='django-insecure-(qo0^+23+zk_abi*xgywz=gdw=qc+lr4^n**pv82yzoo&1i!f4' #os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', False)
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
